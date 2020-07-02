0059f6cd36c306aeceef5bff60161b95b6c7f2fb
antonlazurko <lazurkoanton87@gmail.com>
2020-07-02 10:23:18 +0300

    фиксит бэекдроп и всплывающее меню

--- js/modal.js
+++ js/modal.js
@@ -1,4 +1,18 @@
 (() => {
+  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
+
+  menuBtnRef.addEventListener('click', () => {
+    const expanded =
+      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+    menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+    mobileMenuRef.classList.toggle('is-open');
+  });
+})();
+(() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
@@ -28,18 +42,4 @@
     document.body.classList.toggle('modal-open');
     refs.modal.classList.toggle('is-hidden');
   }
-})();
-(() => {
-  const menuBtnRef = document.querySelector('[data-menu-button]');
-  const mobileMenuRef = document.querySelector('[data-menu]');
-
-  menuBtnRef.addEventListener('click', () => {
-    const expanded =
-      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
-
-    menuBtnRef.classList.toggle('is-open');
-    menuBtnRef.setAttribute('aria-expanded', !expanded);
-
-    mobileMenuRef.classList.toggle('is-open');
-  });
 })();
